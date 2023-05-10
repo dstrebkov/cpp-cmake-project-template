aa11a565fd39a754ea47df0b3de89b8fbca35832
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2023-05-10 23:44:53 +0400

    Simplified Coverage CTest functionality

--- cmake/CTestConfig.cmake
+++ cmake/CTestConfig.cmake
@@ -1,4 +1,3 @@
-set(CTEST_PROJECT_NAME "TerminalApps")
-set(CTEST_SUBMIT_URL 
-    "http://localhost:8080/submit.php?project=${CTEST_PROJECT_NAME}")
-set(CTEST_USE_LAUNCHERS YES)
+#set(CTEST_PROJECT_NAME "CMakeProjectTemplate")
+#set(CTEST_SUBMIT_URL "http://localhost:8080/submit.php?project=${CTEST_PROJECT_NAME}")
+#set(CTEST_USE_LAUNCHERS YES)
