00fe130e704da765f1e9123f983b5e7b461c0b74
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2023-05-11 14:12:43 +0400

    Use gcovr in build pipeline

--- cmake/Pipeline.cmake
+++ cmake/Pipeline.cmake
@@ -16,10 +16,19 @@ ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
 
 ctest_start(Experimental)
 ctest_configure(OPTIONS "-DCMAKE_CXX_FLAGS_INIT=--coverage")
-#ctest_submit(PARTS Start Configure)
 ctest_build()
-#ctest_submit(PARTS Build)
 ctest_test()
-#ctest_submit(PARTS Test)
 ctest_coverage()
-#ctest_submit(PARTS Coverage Done)
+
+find_package(Python3 COMPONENTS Interpreter REQUIRED)
+
+set(REQUIREMENTS_TXT_DIR "${CMAKE_CURRENT_SOURCE_DIR}/factorial_doctest_fakeit/test/")
+set(VENV_PATH "${CTEST_BINARY_DIRECTORY}/py_env")
+set(GCOVR_EXCLUDES_RE ".*/\(_deps|test|CMakeFiles)/")
+
+execute_process(COMMAND "${Python3_EXECUTABLE}" -m venv "${VENV_PATH}")
+execute_process(COMMAND ${VENV_PATH}/bin/python -m pip install --upgrade pip --no-warn-script-location)
+#execute_process(COMMAND ${VENV_PATH}/bin/pip install -r ${REQUIREMENTS_TXT_DIR}/requirements.txt)
+execute_process(COMMAND ${VENV_PATH}/bin/pip install gcovr==6.0)
+#execute_process(COMMAND ${VENV_PATH}/bin/gcovr --use-gcov-files --exclude .*/\(_deps|test\)/ --html-details coverage.html)
+execute_process(COMMAND ${VENV_PATH}/bin/gcovr --root ${CTEST_SOURCE_DIRECTORY} --exclude ${GCOVR_EXCLUDES_RE} --html-details coverage.html)
