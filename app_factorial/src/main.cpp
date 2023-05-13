97df42dfb43db86217d9d3fef68e5e8474e8eaa1
Dmitry Strebkov <dmitry.strebkov@gmail.com>
2023-05-13 15:25:08 +0400

    Update main.cpp

--- app_factorial/src/main.cpp
+++ app_factorial/src/main.cpp
@@ -5,5 +5,6 @@ int main() {
     std::cout << "[Doctest+FakeIt]: factorial(1) = " << factorial_doctest_fakeit::factorial(1) << std::endl;
     std::cout << "[Doctest+FakeIt]: factorial(2) = " << factorial_doctest_fakeit::factorial(2) << std::endl;
     std::cout << "[Doctest+FakeIt]: factorial(3) = " << factorial_doctest_fakeit::factorial(3) << std::endl;
+    std::cout << "[Doctest+FakeIt]: factorial(4) = " << factorial_doctest_fakeit::factorial(4) << std::endl;
     return EXIT_SUCCESS;
 }
